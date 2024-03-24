a487032fa7c655650a5b8ad9367b9db2f8f77949
Admin <Admin@DESKTOP-H3G0PQ1>
2024-03-24 05:37:36 -0800

    commit

--- CaseTools/src/Programs/Fibonacci.java
+++ CaseTools/src/Programs/Fibonacci.java
@@ -1,26 +1,25 @@
 package Programs;
 import java.util.Scanner;
-
 public class Fibonacci {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		 Scanner scanner = new Scanner(System.in);
-	        System.out.print("Enter the number of terms: ");
-	        int n = scanner.nextInt();
-	        System.out.println("Fibonacci Series:");
-	        displayFibonacci(n);
-	    }
+		Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of terms: ");
+        int n = scanner.nextInt();
+        System.out.println("Fibonacci Series:");
+        displayFibonacci(n);
+    }
 
-	    public static void displayFibonacci(int n) {
-	        int firstTerm = 0, secondTerm = 1;
-	        for (int i = 1; i <= n; i++) {
-	            System.out.print(firstTerm + " ");
-	            int nextTerm = firstTerm + secondTerm;
-	            firstTerm = secondTerm;
-	            secondTerm = nextTerm;
-	        }
-	    }
+    public static void displayFibonacci(int n) {
+        int firstTerm = 0, secondTerm = 1;
+        for (int i = 1; i <= n; i++) {
+            System.out.print(firstTerm + " ");
+            int nextTerm = firstTerm + secondTerm;
+            firstTerm = secondTerm;
+            secondTerm = nextTerm;
+        }
+    }
 
 	}
 
